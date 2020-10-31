f8ec7b9489d83695a1009848b8b079b7464a8f0b
João Vitor Néias <agsuns@gmail.com>
2020-10-31 20:24:08 -0300

    final flips

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+window.onload = () =>{
+    let cards = document.querySelectorAll(".card");
 
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", (e) => {
+            let children = cards[i].children;
+ 
+            for (let i = 0; i < children.length; i++) {
+                
+                if (children[i].classList.contains("visible")) {                    
+                    children[i].classList.remove("visible");
 
+                } else {
+                    children[i].classList.add("visible");                    
+                }
+            }
+        })
+    }
+}
